7b38e874edae605d5eccd7036826cba3c09af792
Masataka Koguchi <masakoguchi@vestainc.jp>
2025-09-24 11:36:56 +0900

    edit action

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/remindflow/',
+  base: '/remindFlow/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
